1e99fc9a81a69574ced337684af48f55ab38ee63
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-18 19:52:47 -0500

    feat: add cors validation and change signup http method

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("./middlewares/cors");
 const { createUser } = require("./controllers/users");
 const app = express();
 const PORT = 3001;
@@ -20,7 +21,9 @@ app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
 
-app.use("/signup", createUser);
+app.use(cors);
+
+app.post("/signup", createUser);
 
 app.get("/test", (req, res) => {
   res.send({ message: "ok" });
---
+++ src/middlewares/cors.js
@@ -0,0 +1,19 @@
+module.exports = (req, res, next) => {
+  const allowedDomains = ["http://localhost:3000", "http://127.0.0.1:3000"];
+  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = "GET, HEAD, PUT, PATCH, POST, DELETE";
+  if (allowedDomains.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header(
+      "Access-Control-Allow-Headers",
+      req.headers["access-control-request-headers"]
+    );
+    return res.end();
+  }
+
+  next();
+};
