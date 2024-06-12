7df88b90baded2ac3ea4d6ef590c44295d20d624
José Espinoza <jose.espinoza.3d@gmail.com>
2024-06-12 04:15:31 -0500

    feat: add http error status for the error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,3 +1,7 @@
+const { HttpStatus } = require("../enums/http");
+
 module.exports = (err, req, res, next) => {
-  res.json({ message: err.message });
+  res
+    .status(err.status || HttpStatus.INTERNAL_SERVER_ERROR)
+    .json({ message: err.message });
 };
