9bb6993ce81f044e5520ef539736487be707bb2c
José Espinoza <jose.espinoza.3d@gmail.com>
2024-06-23 08:46:37 -0500

    feat: add public domains to cors list

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,6 +3,10 @@ module.exports = (req, res, next) => {
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
+    "http://find.utdnews.com",
+    "https://find.utdnews.com",
+    "http://www.find.utdnews.com",
+    "https://www.find.utdnews.com",
   ];
   const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = "GET, HEAD, PUT, PATCH, POST, DELETE";
